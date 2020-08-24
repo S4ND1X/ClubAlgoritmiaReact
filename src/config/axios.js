02a7e4e7a6c17de7563ef5e7bf4d09e88eed4916
S4NDIX <sandix390@gmail.com>
2020-08-23 23:32:41 -0500

    Trying cors auth

--- src/config/axios.js
+++ src/config/axios.js
@@ -3,9 +3,6 @@ import axios from "axios";
 //Assign base URL for api fetch
 const clientAxios = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
-  headers: {
-    origin: process.env.REACT_APP_ORIGIN_ACCESS,
-  },
 });
 
 export default clientAxios;
